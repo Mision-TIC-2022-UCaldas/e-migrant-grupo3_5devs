078647d3efe0a3e01ec1a14c0bbeffac2bfa44f7
Joan Cortes <js.199911@gmail.com>
2021-11-26 17:34:18 -0500

    CU-01 HU-01

--- E-Migrant/E-Migrant.App/E-Migrant.App.Frontend/Pages/WebMigrante/RegistroMigrante.cshtml.cs
+++ E-Migrant/E-Migrant.App/E-Migrant.App.Frontend/Pages/WebMigrante/RegistroMigrante.cshtml.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using E_Migrant.App.Dominio.Entidades;
+using E_Migrant.App.Persistencia.appRepositorios;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,8 +11,47 @@ namespace E_Migrant.App.Frontend.Pages
 {
     public class RegistroMigranteModel : PageModel
     {
-        public void OnGet()
+        private static IRepositorioMigrante _repositorioMigrante = new RepositorioMigrante(new appContext());
+        [BindProperty]
+        public Migrante migrante { get; set; }
+        public IActionResult OnGet(int? idMigrante)
         {
+            if (idMigrante.HasValue)
+            {
+                migrante = _repositorioMigrante.GetMigrante(idMigrante.Value);
+            }
+            else
+            {
+                migrante = new Migrante();
+            }
+
+            if (migrante == null)
+            {
+                return RedirectToPage("./ServiciosMigrante");
+            }
+            else
+            {
+                return Page();
+            }
+        }
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            else
+            {
+                if (migrante.Id > 0)
+                {
+                    _repositorioMigrante.UpdateMigrante(migrante);
+                }
+                else
+                {
+                    _repositorioMigrante.AddMigrante(migrante);
+                }
+            }
+            return RedirectToPage("./ServiciosMigrante");
         }
     }
 }
