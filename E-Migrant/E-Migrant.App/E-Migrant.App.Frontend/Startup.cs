a6d116f8e7d37acc20889a717ea8ae8f990c1436
Leandro <lvalenciab@unal.edu.co>
2021-11-27 12:13:51 -0500

    Implementación del Identity

--- E-Migrant/E-Migrant.App/E-Migrant.App.Frontend/Startup.cs
+++ E-Migrant/E-Migrant.App/E-Migrant.App.Frontend/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
 
 namespace E_Migrant.App.Frontend
 {
@@ -23,7 +24,10 @@ namespace E_Migrant.App.Frontend
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            
             services.AddRazorPages();
+            //services.AddDbContext<E_Migrant.App.Persistencia.appRepositorios.appContext>();
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -44,6 +48,7 @@ namespace E_Migrant.App.Frontend
             app.UseStaticFiles();
 
             app.UseRouting();
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
