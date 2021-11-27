67909fabe42eeb1813800fe71dd105d7e17a53fa
Leandro <lvalenciab@unal.edu.co>
2021-11-27 14:56:21 -0500

    Edicion novedad y usuario

--- E-Migrant/E-Migrant.App/E-Migrant.App.Dominio/Entidades/Usuario.cs
+++ E-Migrant/E-Migrant.App/E-Migrant.App.Dominio/Entidades/Usuario.cs
@@ -5,7 +5,6 @@ namespace E_Migrant.App.Dominio.Entidades
     {
         public int Id {get;set;}
         public string Username {get;set;}
-        public string Password {get;set;}
         public Rol rol {get;set;}
     }
 }
